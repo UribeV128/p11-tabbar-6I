de4ff482ab8e7dcd73b658b033eb4aa97e335ff6
VIVIAN_URIBE_MENDOZA <a.21308051280418@cbtis128.edu.mx>
2024-02-29 19:00:07 +0000

    actualizado

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,85 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(AppMiTapBar());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class AppMiTapBar extends StatelessWidget {
+  const AppMiTapBar({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: true,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      title: "Ejemplo Tabbar Vivian Uribe",
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: MiPaginaInicial(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+//stateful
+class MiPaginaInicial extends StatefulWidget {
+  const MiPaginaInicial({Key? key}) : super(key: key);
 
   @override
+  State<MiPaginaInicial> createState() => _MiPaginaInicialState();
+} //MiPaginaInicial
+
+class _MiPaginaInicialState extends State<MiPaginaInicial> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("Tabbar Vivian Uribe"),
+          centerTitle: true,
+          bottom: TabBar(tabs: [
+            Tab(
+              text: "Facial",
+              icon: Icon(Icons.face_retouching_natural),
+            ),
+            Tab(
+              text: "Cuerpo",
+              icon: Icon(Icons.boy),
+            ),
+            Tab(
+              text: "Cabello",
+              icon: Icon(Icons.face_2),
+            ),
+            Tab(
+              text: "otros",
+              icon: Icon(Icons.auto_awesome),
+            ),
+          ] //fin de tabs
+              ), //fin bottom tabbar
         ),
+        body: TabBarView(children: const <Widget>[
+          Center(
+            child: Text(
+              "Skin care",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 40),
+            ),
+          ),
+          Center(
+            child: Text(
+              "Productos para el cuerpo",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+            ),
+          ),
+          Center(
+            child: Text(
+              "Productos para el cabello",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+            ),
+          ),
+          Center(
+            child: Text(
+              "Productos ",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+            ),
+          ),
+        ]), // fin de tabbview
       ),
-    );
-  }
-}
+    ); //DefaultTabController
+  } //fin widget
+} //_MiPaginaInicialState
